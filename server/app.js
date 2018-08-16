649d6a57117ece6979136cab516986a3730ddba7
Luke Camelo <azathothbeats@gmail.com>
2018-08-16 18:33:46 -0400

    Add lodash, write resolve function

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({}))
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema
+  })
+)
 
 app.listen(8080, () => {
   console.log('reporting live from localhost:/8080 :-)')
