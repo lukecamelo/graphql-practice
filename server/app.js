b8182afd6dba2d76fb1f89a56a9c2910f4d15782
Luke Camelo <azathothbeats@gmail.com>
2018-08-16 18:21:21 -0400

    Wrote graphql schema

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({}))
+
+app.listen(8080, () => {
+  console.log('reporting live from localhost:/8080 :-)')
+})
