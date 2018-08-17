2efe8534ba29ca02bbe96439a68f1e73bc7bffbd
Luke Camelo <azathothbeats@gmail.com>
2018-08-16 22:49:40 -0400

    Apollo client used to display books from database

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+// allow cross-origin requests
+app.use(cors())
 
 mongoose.connect(
   'mongodb://lukecamelo:pandab3ar@ds223542.mlab.com:23542/gql-books',
