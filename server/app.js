2d158f7020f64213bd110db43c9dece10dd8a8f9
Luke Camelo <azathothbeats@gmail.com>
2018-08-16 20:55:47 -0400

    Connect to MongoDB database, add GraphQL mutations

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(
+  'mongodb://lukecamelo:pandab3ar@ds223542.mlab.com:23542/gql-books',
+  {
+    useNewUrlParser: true
+  }
+)
+mongoose.connection.once('open', () => {
+  console.log('connected to database!')
+})
 
 app.use(
   '/graphql',
@@ -13,5 +24,5 @@ app.use(
 )
 
 app.listen(8080, () => {
-  console.log('reporting live from localhost:/8080 :-)')
+  console.log('reporting live from localhost:8080 :-)')
 })
